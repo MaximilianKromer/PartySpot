b7683e9c59c3e7f4f6215bcc0192b69dd34d4b8d
Maximilian Kromer <a.makro@web.de>
2020-08-18 17:41:34 +0200

    adding navigation

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View } from 'react-native';
+import ListTab from './tabs/ListTab';
+import MapTab from './tabs/MapTab';
+import FavouritesTab from './tabs/FavouritesTab';
+import SettingsTab from './tabs/SettingsTab';
+import { Ionicons } from '@expo/vector-icons';
+import { 
+    OpenSans_300Light,
+    OpenSans_300Light_Italic,
+    OpenSans_400Regular,
+    OpenSans_400Regular_Italic,
+    OpenSans_600SemiBold,
+    OpenSans_600SemiBold_Italic,
+    OpenSans_700Bold,
+    OpenSans_700Bold_Italic,
+    OpenSans_800ExtraBold,
+    OpenSans_800ExtraBold_Italic, 
+    useFonts
+  } from '@expo-google-fonts/open-sans'
+import { AppLoading } from 'expo';
+import { colors } from './styles/Global';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    let [fontsLoaded] = useFonts({
+        OpenSans_300Light,
+        OpenSans_400Regular,
+        OpenSans_600SemiBold,
+        OpenSans_700Bold,
+        OpenSans_800ExtraBold,
+      });
+
+    const Tab = createBottomTabNavigator();
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return (
+            <NavigationContainer>
+                <Tab.Navigator
+                    backBehavior="history"
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
+
+                            if (route.name === 'List') {
+                                iconName = focused ? 'ios-list-box' : 'ios-list';
+                            } else if (route.name === 'Map') {
+                                iconName = 'ios-map';
+                            } else if (route.name === 'Favourites') {
+                                iconName = focused ? 'ios-star' : 'ios-star-outline';
+                            } else if (route.name === 'Settings') {
+                                iconName = 'ios-settings';
+                            }
+                            return <Ionicons name={iconName} size={size} color={color} />;
+                        },
+                    })}
+                    tabBarOptions={{
+                        activeTintColor: colors.white,
+                        inactiveTintColor: colors.white,
+                        activeBackgroundColor: colors.primary,
+                        inactiveBackgroundColor: colors.primary,
+                        showLabel: false
+
+                    }}
+                >
+                    <Tab.Screen name="List" title="Events" component={ListTab} tabBarTestID="1" initialParams={{ title: "Events" }}/>
+                    <Tab.Screen name="Map" title="Karte" component={MapTab} tabBarTestID="2" initialParams={{ title: "Karte" }}/>
+                    <Tab.Screen name="Favourites" title="Favoriten" component={FavouritesTab} tabBarTestID="3" initialParams={{ title: "Favoriten" }}/>
+                    <Tab.Screen name="Settings" title="Einstellungen" component={SettingsTab} tabBarTestID="4" initialParams={{ title: "Einstellungen" }}/>
+                </Tab.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+      flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
