d6753552dcee6a0620f53ee6691182d9cecb0ed8
Maximilian Kromer <a.makro@web.de>
2020-08-19 21:28:11 +0200

    updated imports

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import ListTab from './tabs/ListTab';
 import MapTab from './tabs/MapTab';
 import FavouritesTab from './tabs/FavouritesTab';
