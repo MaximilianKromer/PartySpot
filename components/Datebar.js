4534a7245abf9d4f6f9ad2f36cde025e527806c0
Maximilian Kromer <a.makro@web.de>
2020-08-20 19:50:58 +0200

    update date display format

--- components/Datebar.js
+++ components/Datebar.js
@@ -3,17 +3,20 @@ import { View, Text } from 'react-native';
 import { HeaderStyles } from '../styles/SpecialComponents';
 import { Ionicons } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { subDays, addDays, format } from 'date-fns';
+import { de } from 'date-fns/locale';
 
 export default function Datebar(props) {
-    const [day, setDay] = useState(14)
+    const [date, setDate] = useState(new Date());
+    const options = { locale: de };
 
     return (
         <View style={[HeaderStyles.header, HeaderStyles.dateBar]}>
-            <TouchableOpacity style={{paddingHorizontal: 12, paddingVertical: 1, }} onPress={() => setDay(day - 1)}>
+            <TouchableOpacity style={{paddingHorizontal: 12, paddingVertical: 1, }} onPress={() => setDate(subDays(date, 1))}>
                 <Ionicons name="ios-arrow-back" size={28} color="black" />
             </TouchableOpacity>
-            <Text style={HeaderStyles.title}>{day}. August</Text>
-            <TouchableOpacity style={{paddingHorizontal: 12, paddingVertical: 1, }} onPress={() => setDay(day + 1)}>
+            <Text style={HeaderStyles.title}>{format(date, 'eeeeee, d. MMMM', options)}</Text>
+            <TouchableOpacity style={{paddingHorizontal: 12, paddingVertical: 1, }} onPress={() => setDate(addDays(date, 1))}>
                 <Ionicons name="ios-arrow-forward" size={28} color="black" />
             </TouchableOpacity>
         </View>
