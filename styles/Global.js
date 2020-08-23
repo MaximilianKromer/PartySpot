8a7bd344c728926a66d1bd25df215063dd996397
Maximilian Kromer <a.makro@web.de>
2020-08-23 21:58:58 +0200

    added new color

--- styles/Global.js
+++ styles/Global.js
@@ -5,6 +5,7 @@ export const colors = {
     white: '#FFFFFF',
     black: '#000000',
     lightGrey: '#F0F0F0', // E6E9E9
+    darkGray: '#999999',
     primary: '#607D8B', // FFCA36
     accent: '#',
     lightText: '#FEFEFE',
