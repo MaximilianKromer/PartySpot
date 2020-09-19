4dcf6c4055e8887dfb9f6c554b2e361cbc70b2be
Maximilian Kromer <a.makro@web.de>
2020-09-20 00:43:46 +0200

    showing favourites

--- tabs/FavouritesTab.js
+++ tabs/FavouritesTab.js
@@ -5,54 +5,32 @@ import Header from '../components/Header';
 import EventItem from '../components/EventItem';
 import SectionHeader from '../components/SectionHeader';
 import Separator from '../components/Separator';
+import { useSelector } from 'react-redux';
 
 
 export default function FavouritesTab(props) {
 
-    const events = [
-        {
-            date: '14. August',
-            data: [
-                {
-                    id: 1,
-                    name: 'Ringbahnsaufen',
-                    location: 'U-Bahnhof Schlosspark',
-                    distance: 12,
-                    time: 'Ab 22 Uhr',
-                    tags: ['Ab 16', 'Park'],
-                    description: 'Lorem Ipsum dsafjk as sdahf. sdwiob av afiensa f asdfajsdf'
-                }, {
-                    id: 2,
-                    name: 'Geburtstag am Lietzensee',
-                    location: 'Lietzensee',
-                    distance: 7,
-                    time: 'Ab 18 Uhr',
-                    tags: ['Club', '4Free'],
-                    description: 'Lorem Ipsum dsafjk as sdahf. sdwiob av afiensa f asdfajsdf'
-                },
-            ]
-        }, {
-            date: '23. August',
-            data: [
-                {
-                    id: 1,
-                    name: 'Ringbahnsaufen',
-                    location: 'U-Bahnhof Schlosspark',
-                    distance: 12,
-                    time: 'Ab 22 Uhr',
-                    tags: ['4Free'],
-                    description: 'Lorem Ipsum dsafjk as sdahf. sdwiob av afiensa f asdfajsdf'
-                },
-            ]
-        },
-    ];
+    const eventsRaw = useSelector(state => state.events.favourites);
+
+    let eventsList = [];
+    eventsRaw.forEach(event => {
+        const index = eventsList.findIndex(el => el.date == event.dateString);
+        if (index >= 0) {
+            eventsList[index].data.push(event);
+        } else {
+            eventsList.push({date: event.dateString, data: [event]});
+        }
+    });
+
+    eventsList = eventsList.length ? eventsList : [{ date: 'Keine Favoriten', data: [] }];
+
 
     return (
         <SafeAreaView style={{flex: 1}}>
             <View style={{flex: 1, backgroundColor: '#fff'}}>
                 <Header title='Favoriten'/>
                 <SectionList
-                    sections={events}
+                    sections={eventsList}
                     refreshing={false}
                     renderItem={({ item }) => <EventItem event={item} />}
                     renderSectionHeader={({ section: { date } }) => (
