41a90ee871bbf76493ec4da0e657676cb75ed82c
Maximilian Kromer <a.makro@web.de>
2020-09-27 17:16:16 +0200

    fix disabled prop

--- components/CardButton.js
+++ components/CardButton.js
@@ -7,12 +7,20 @@ export default function CardButton(props) {
     const filledStyle = props.filled ? CardStyle.filled : null;
     const filledTextStyle = props.filled ? CardStyle.btnfilledtext : null;
     const dangerStyle = props.danger ? CardStyle.danger : null;
-    const disabledStyle = props.disabled ? CardStyle.disabled : null;
 
     const text = props.text ? <Text style={[CardStyle.btntext, filledTextStyle, dangerStyle]}>{props.text}</Text> : null;
 
+    if (props.disabled) {
+        return (
+            <TouchableOpacity style={[CardStyle.card, filledStyle, CardStyle.disabled]}>
+                {text}
+                {props.children}
+            </TouchableOpacity>
+        );
+    }
+
     return (
-        <TouchableOpacity style={[CardStyle.card, filledStyle, disabledStyle]} onPress={props.onPress}>
+        <TouchableOpacity style={[CardStyle.card, filledStyle]} onPress={props.onPress}>
             {text}
             {props.children}
         </TouchableOpacity>
